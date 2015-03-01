aa6292fb81bd8a7f254825455835e98cc42b498f
Jason White <jasonaw0@gmail.com>
2015-02-28 16:50:32 -0800

    Move rule parsing to new module.

--- source/app.d
+++ source/app.d
@@ -1,96 +1,5 @@
 import io;
-
-struct Rule
-{
-    string[] inputs;
-    string[] outputs;
-    string task;
-    string description;
-}
-
-struct Rules
-{
-    import std.json;
-
-    private
-    {
-        JSONValue[] rules;
-
-        // Current rule taken from the stream.
-        Rule rule;
-
-        bool _empty;
-    }
-
-    this(JSONValue rules)
-    {
-        this.rules = rules.array();
-
-        // Prime the cannon
-        popFront();
-    }
-
-    void popFront()
-    {
-        import std.range : empty, popFront, front;
-        import std.algorithm : map;
-        import std.array : array;
-
-        if (rules.empty)
-        {
-            _empty = true;
-            return;
-        }
-
-        auto jsonRule = rules.front;
-
-        auto inputs = jsonRule["inputs"].array().map!(x => x.str()).array();
-        auto outputs = jsonRule["outputs"].array().map!(x => x.str()).array();
-        auto task = jsonRule["task"].str();
-
-        // Optional description
-        string description = null;
-
-        try
-        {
-            description = jsonRule["description"].str();
-        }
-        catch (JSONException e)
-        {
-            // Ignore. Description is optional.
-        }
-
-        rule = Rule(inputs, outputs, task, description);
-
-        rules.popFront();
-    }
-
-    inout(Rule) front() inout
-    {
-        return rule;
-    }
-
-    bool empty() const pure nothrow
-    {
-        return _empty;
-    }
-}
-
-/**
- * Convenience function for constructing a Rules range.
- */
-Rules parseRules(Stream)(Stream stream)
-    if (isSource!Stream)
-{
-    import std.json : parseJSON;
-    return Rules(stream.byBlock!char.parseJSON()["rules"]);
-}
-
-/// Ditto
-Rules parseRules(string fileName)
-{
-    return parseRules(File(fileName, FileFlags.readExisting));
-}
+import bb.rule;
 
 /**
  * Bipartite task graph.
@@ -108,14 +17,20 @@ struct TaskGraph
     void addRules()(auto ref Rules rules)
     {
         foreach (rule; rules)
-        {
-            // TODO
-            println("Inputs:      ", rule.inputs);
-            println("Outputs:     ", rule.outputs);
-            println("Task:        ", rule.task);
-            println("Description: ", rule.description);
-            println();
-        }
+            addRule(rule);
+    }
+
+    /**
+     * Adds a single rule to the graph.
+     */
+    void addRule()(auto ref Rule rule)
+    {
+        // TODO
+        println("Inputs:      ", rule.inputs);
+        println("Outputs:     ", rule.outputs);
+        println("Task:        ", rule.task);
+        println("Description: ", rule.description);
+        println();
     }
 }
 
---
+++ source/bb/rule.d
@@ -0,0 +1,106 @@
+/**
+ * Copyright: Copyright Jason White, 2015
+ * License:   $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * Authors:   Jason White
+ *
+ * Description:
+ * Parses rules.
+ */
+module bb.rule;
+
+import io.stream.types : isSource;
+
+struct Rule
+{
+    string[] inputs;
+    string[] outputs;
+    string task;
+    string description;
+}
+
+struct Rules
+{
+    import std.json : JSONValue;
+
+    private
+    {
+        JSONValue[] rules;
+
+        // Current rule taken from the stream.
+        Rule rule;
+
+        bool _empty;
+    }
+
+    this(JSONValue rules)
+    {
+        this.rules = rules.array();
+
+        // Prime the cannon
+        popFront();
+    }
+
+    void popFront()
+    {
+        import std.range : empty, popFront, front;
+        import std.algorithm : map;
+        import std.array : array;
+        import std.json : JSONException;
+
+        if (rules.empty)
+        {
+            _empty = true;
+            return;
+        }
+
+        auto jsonRule = rules.front;
+
+        auto inputs = jsonRule["inputs"].array().map!(x => x.str()).array();
+        auto outputs = jsonRule["outputs"].array().map!(x => x.str()).array();
+        auto task = jsonRule["task"].str();
+
+        // Optional description
+        string description = null;
+
+        try
+        {
+            description = jsonRule["description"].str();
+        }
+        catch (JSONException e)
+        {
+            // Ignore. Description is optional.
+        }
+
+        rule = Rule(inputs, outputs, task, description);
+
+        rules.popFront();
+    }
+
+    inout(Rule) front() inout
+    {
+        return rule;
+    }
+
+    bool empty() const pure nothrow
+    {
+        return _empty;
+    }
+}
+
+/**
+ * Convenience function for constructing a Rules range.
+ */
+Rules parseRules(Stream)(Stream stream)
+    if (isSource!Stream)
+{
+    import std.json : parseJSON;
+    import io.range : byBlock;
+    return Rules(stream.byBlock!char.parseJSON()["rules"]);
+}
+
+/// Ditto
+Rules parseRules(string fileName)
+{
+    import io.file;
+    return parseRules(File(fileName, FileFlags.readExisting));
+}
