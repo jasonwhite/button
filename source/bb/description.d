79f998b2fc3ea99c24bdcc0fa2850b43467f8015
Jason White <jasonaw0@gmail.com>
2015-07-03 15:18:12 -0700

    Add build description diffing.

--- source/bb/description.d
+++ source/bb/description.d
@@ -8,10 +8,10 @@
  */
 module bb.description;
 
-import bb.rule, bb.vertex, bb.change, bb.state;
-import multiset;
+import change, multiset;
+import bb.rule, bb.vertex, bb.state;
 import std.typecons : Tuple, tuple;
-import std.array : Appender;
+import std.range : ElementType;
 
 /**
  * Reads a build description.
@@ -20,34 +20,59 @@ struct BuildDescription
 {
     private
     {
+        import std.array : Appender;
+
         // Vertices
-        Appender!(Resource.Id[]) resources;
-        Appender!(Task.Id[]) tasks;
+        Appender!(ResourceId[]) resources;
+        Appender!(TaskId[]) tasks;
 
         // Edges
-        Appender!(Tuple!(Resource.Id, Task.Id)[]) edgesRT; // Resource -> Task
-        Appender!(Tuple!(Task.Id, Resource.Id)[]) edgesTR; // Task -> Resource
+        Appender!(Tuple!(ResourceId, TaskId)[]) edgesRT; // Resource -> Task
+        Appender!(Tuple!(TaskId, ResourceId)[]) edgesTR; // Task -> Resource
     }
 
-    this()(auto ref Rules rules) pure
+    this(R)(auto ref R rules) pure
+        if (is(ElementType!R : const(Rule)))
     {
+        import std.algorithm : sort;
+
         foreach (rule; rules)
             addRule(rule);
 
-        // TODO: Sort arrays.
+        sort(resources.data);
+        sort(tasks.data);
+        sort(edgesRT.data);
+        sort(edgesTR.data);
     }
 
-    this(BuildState state) pure
+    this(BuildState state)
     {
-        //add(state);
+        import std.algorithm : sort;
 
-        // TODO: Sort arrays.
+        // TODO: Add all nodes and edges.
+        // TODO: Only add explicit edges.
+
+        // Add all nodes.
+        foreach (id; state.taskIdentifiers)
+            tasks.put(id);
+        foreach (id; state.resourceIdentifiers)
+            resources.put(id);
+
+        // Add all edges.
+        //foreach (edge; state.resourceEdges)
+            //edgesRT.put(tuple(edge.from, edge.to));
+
+        //foreach (edge; state.taskEdges)
+            //edgesTR.put(tuple(edge.from, edge.to));
+
+        sort(resources.data);
+        sort(tasks.data);
     }
 
     /**
      * Adds a single rule.
      */
-    void addRule()(auto ref Rule rule)
+    private void addRule()(auto const ref Rule rule)
     {
         tasks.put(rule.task);
 
@@ -67,17 +92,60 @@ struct BuildDescription
     /**
      * Returns the differences between this build description and another.
      */
-    auto diff(const ref BuildDescription rhs) const pure
+    auto diff()(auto ref BuildDescription rhs) pure
     {
-        import std.algorithm : sort, uniq;
+        import std.algorithm : uniq, filter;
         import std.array : array;
         import std.typecons : tuple;
 
         return tuple(
-            changes(this.resources.data.uniq, rhs.resources.data.uniq),
-            changes(this.tasks.data.uniq, rhs.tasks.data.uniq),
-            changes(this.edgesRT.data.uniq, rhs.edgesRT.data.uniq),
-            changes(this.edgesTR.data.uniq, rhs.edgesTR.data.uniq)
+            changes(this.resources.data.uniq, rhs.resources.data.uniq).filter!(c => c.type != ChangeType.none),
+            changes(this.tasks.data.uniq, rhs.tasks.data.uniq).filter!(c => c.type != ChangeType.none),
+            changes(this.edgesRT.data.uniq, rhs.edgesRT.data.uniq).filter!(c => c.type != ChangeType.none),
+            changes(this.edgesTR.data.uniq, rhs.edgesTR.data.uniq).filter!(c => c.type != ChangeType.none)
             );
     }
+}
+
+unittest
+{
+    immutable Rule[] rulesA = [
+        {
+            inputs: ["foo.c", "baz.h"],
+            task: ["gcc", "-c", "foo.c", "-o", "foo.o"],
+            outputs: ["foo.o"]
+        },
+        {
+            inputs: ["bar.c", "baz.h"],
+            task: ["gcc", "-c", "bar.c", "-o", "bar.o"],
+            outputs: ["bar.o"]
+        },
+        {
+            inputs: ["foo.o", "bar.o"],
+            task: ["gcc", "foo.o", "bar.o", "-o", "foobar"],
+            outputs: ["foobar"]
+        },
+    ];
+
+    immutable Rule[] rulesB = [
+        {
+            inputs: ["foo.c", "baz.h"],
+            task: ["gcc", "-c", "foo.c", "-o", "foo.o"],
+            outputs: ["foo.o"]
+        },
+        {
+            inputs: ["bar.c", "baz.h"],
+            task: ["gcc", "-c", "bar.c", "-o", "bar.o"],
+            outputs: ["bar.o"]
+        },
+    ];
+
+    import io;
+
+    auto diff = BuildDescription(rulesA).diff(BuildDescription(rulesB));
+
+    println("Resource diff: ", diff[0]);
+    println("Task diff:     ", diff[1]);
+    println("RT diff:       ", diff[2]);
+    println("TR diff:       ", diff[3]);
 }
