2752b944d6f393ed64ca4f256fd88c847d926e5d
Jason White <jasonaw0@gmail.com>
2015-08-01 15:18:43 -0700

    Add additional documentation on edges.

--- source/bb/edge.d
+++ source/bb/edge.d
@@ -6,13 +6,29 @@
 module bb.edge;
 
 /**
- * A resource can be explicitly specified by the build description. That is, the
- * edge connecting a task and resource was added by the user. Otherwise, if the
- * edge was added by the build system, it is an implicit resource.
+ * Type of an edge.
  */
 enum EdgeType
 {
+    /**
+     * An explicit edge is one that was specified in the build description.
+     */
     explicit,
+
+    /**
+     * An implicit edge is one that is reported by a task.
+     *
+     * Ideally, the set of implicit edges should always be a superset of the set
+     * of explicit edges. If this is not the case, it implies one of two
+     * problems:
+     *
+     *  1. The task is not reporting all dependencies.
+     *  2. A superfluous dependency is specified in the build description.
+     *
+     * An over-specification of dependencies will not lead to a build error,
+     * merely over-building. Thus, such a situation should only be reported as a
+     * warning.
+     */
     implicit,
 }
 
