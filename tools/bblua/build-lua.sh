254e6229c469587867b18e7bbec87d77d86b7567
Jason White <jasonaw0@gmail.com>
2015-12-09 20:37:10 -0800

    Add Lua build script

---
+++ tools/bblua/build-lua.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Downloads and builds Lua.
+
+VERSION=5.3.2
+SHA256SUM=c740c7bb23a936944e1cc63b7c3c5351a8976d7867c5252c8854f7b2af9da68f
+
+# TODO: Determine OS automatically with uname
+OS=linux
+
+if [[ -n "$1" ]]; then
+    OS=$1
+fi
+
+if [[ ! -f lua-$VERSION.tar.gz ]]; then
+    curl -R -O http://www.lua.org/ftp/lua-$VERSION.tar.gz || exit $?
+    echo "$SHA256SUM  lua-$VERSION.tar.gz" | sha256sum --check || exit $?
+fi
+
+if [[ ! -d lua-$VERSION ]]; then
+    tar zxf lua-$VERSION.tar.gz || exit $?
+fi
+
+cd lua-$VERSION
+make $OS test || exit $?
