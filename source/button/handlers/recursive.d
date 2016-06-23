60e823e1347b757ace955566798c84a7162fac58
Jason White <jasonaw0@gmail.com>
2016-06-23 00:21:15 -0700

    Add in-process recursive builds

--- source/button/handlers/recursive.d
+++ source/button/handlers/recursive.d
@@ -24,11 +24,13 @@
  */
 module button.handlers.recursive;
 
+import std.parallelism : TaskPool;
+
 import button.log;
 import button.resource;
 import button.context;
 import button.build;
-
+import button.state;
 import button.cli;
 
 import darg;
@@ -44,14 +46,98 @@ int execute(
 {
     import button.handlers.base : base = execute;
 
+    import std.path : dirName, absolutePath, buildPath;
+
     auto globalOpts = parseArgs!GlobalOptions(args[1 .. $], Config.ignoreUnknown);
-    auto buildOpts  = parseArgs!BuildOptions(globalOpts.args);
+    auto opts = parseArgs!BuildOptions(globalOpts.args);
 
     // Not the build command, forward to the base handler.
     if (globalOpts.command != "build")
         return base(ctx, args, workDir, inputs, outputs, logger);
 
-    // TODO: Get the build state database and run the build
+    string path;
 
-    return base(ctx, args, workDir, inputs, outputs, logger);
+    if (opts.path.length)
+        path = buildPath(workDir, opts.path);
+    else
+        path = buildDescriptionPath(workDir);
+
+    auto state = new BuildState(path.stateName);
+
+    // Reuse as much of the parent build context as possible.
+    auto newContext = BuildContext(
+            path.dirName.absolutePath,
+            ctx.pool, ctx.logger, state,
+            ctx.dryRun, ctx.verbose, ctx.color
+            );
+
+    state.begin();
+
+    scope (exit)
+    {
+        if (newContext.dryRun)
+            state.rollback();
+        else
+            state.commit();
+    }
+
+    syncBuildState(state, newContext.pool, path);
+
+    // TODO: Get changes from the parent build system because this is
+    // duplicating work that has already been done.
+    queueChanges(state, newContext.pool, newContext.color);
+
+    // Do the build.
+    update(newContext);
+
+    // Publish implicit resources to parent build
+    foreach (v; state.enumerate!(Index!Resource))
+    {
+        immutable degreeIn  = state.degreeIn(v);
+        immutable degreeOut = state.degreeOut(v);
+
+        if (degreeIn == 0 && degreeOut == 0)
+            continue; // Dangling resource
+
+        if (degreeIn == 0)
+            inputs.put(state[v]);
+        else
+            outputs.put(state[v]);
+    }
+
+    return 0;
+}
+
+/**
+ * Updates the database with any changes to the build description.
+ */
+private void syncBuildState(BuildState state, TaskPool pool, string path)
+{
+    auto r = state[BuildState.buildDescId];
+    r.path = path;
+    if (r.update())
+    {
+        path.syncState(state, pool);
+
+        // Update the build description resource
+        state[BuildState.buildDescId] = r;
+    }
+}
+
+/**
+ * Builds pending vertices.
+ */
+private void update(ref BuildContext ctx)
+{
+    import std.array : array;
+
+    import button.task : Task;
+
+    auto resources = ctx.state.pending!Resource.array;
+    auto tasks     = ctx.state.pending!Task.array;
+
+    if (resources.length == 0 && tasks.length == 0)
+        return;
+
+    ctx.state.buildGraph(resources, tasks).build(ctx);
 }
